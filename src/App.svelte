68fda96bd5c703abb4af8e3964275f4b7e4efe01
minokuma <ksky216@naver.com>
2023-09-13 23:51:30 +0900

    1 binaries uploaded

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
 
 <!-- image backdrop blur -->
 <div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-red-50 py-6 sm:py-12">
-  <img src="/img/haze-graphic-design-creative-color-smoke-f8b74ea1c81cf4dabaa46174290eb5a8.png" alt="" class="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
+  <img src="/img/gradient_texture_line_fluid_3797751.png" alt="" class="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
   <div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
   <div class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
     <div class="mx-auto max-w-md">
