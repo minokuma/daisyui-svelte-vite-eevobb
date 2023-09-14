025a1fca78a2e5797f18b6a896b3602dd78f2fb2
minokuma <ksky216@naver.com>
2023-09-14 10:44:44 +0900

    1 binaries uploaded

--- src/App.svelte
+++ src/App.svelte
@@ -14,8 +14,8 @@
 </script>
 
 <!-- image backdrop blur -->
-<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-red-50 py-6 sm:py-12">
-  <img src="/img/docs-dark@tinypng.1bbe175e.png" alt="" class="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
+<div class="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-70 py-6 sm:py-12">
+  <img src="/img/blue-euclidean-vector-big-waves-blue-lines-background-8fb935eae89d531bc797452e31647189.png" alt="" class="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308" />
   <div class="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
   <div class="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
     <div class="mx-auto max-w-md">
